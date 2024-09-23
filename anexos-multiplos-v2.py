67c1ae755f2876606cbc02b733b4bc495aa9ec7a
Bortoletti <lbortoletti@deltasystems.com.br>
2024-09-23 19:14:45 -0300

    Ajuste

--- anexos-multiplos-v2.py
+++ anexos-multiplos-v2.py
@@ -12,59 +12,83 @@ from email import encoders
 import random
 import time
 
-
-
-server = smtplib.SMTP('smtp-relay.gmail.com', 587)
-server.starttls()  # Iniciar a conexão TLS
-
-# Conectar ao servidor SMTP do Gmail e enviar o e-mail
-conta_email = "luis.bortoletti@gruposouzalima.com"
-password = "Borto!2024#01"
-server.login( conta_email , password)
-
-
+def getRandom():
+   return random.randint(100, 999) 
 
 def getTimestampFmt2():
    now = datetime.now()
-   return now.strftime("%Y-%m-%d-%H:%M")
+   return now.strftime("%Y-%m-%d-%H:%M:%S")
+
+
+class Logger:
+    def __init__( self, fileLogParam ):
+        self.fileLog = fileLogParam
+    
+    def write( self, msg ):
+        with open( self.fileLog, 'a') as logFile:
+            logFile.write( f"{getTimestampFmt2()}: {msg}\n" )
+            logFile.close()
+
+
+
+
+
+#============================== DESTINO    ==============================
 
 class Destino:
-    def __init__(self, nome, email ):
+    def __init__(self, nome, email, chaveParam ):
         self.nome = nome
         self.email = email
+        self.chave = chaveParam
 
     def __str__(self):
-        return f"Destino(nome={self.nome}, email={self.email})"
+        return f"'destino':'chave':'{self.chave}','nome':'{self.nome}', 'email':'{self.email}'"
 
+#============================== ENVIAR EMAIL   ==============================
 
-def sendEmail( fileParam, destinoParam ):
-    global server
+def sendEmail( fileParam, destinoParam, server, logar ):
+    # global server
+    logar.write("-----------------------  INICIO DE ENVIO  ----------------------------")
+    logar.write(f"Arquivo.: {fileParam}")
+    logar.write(f"Chave...: {destinoParam.chave}")
+    logar.write(f"Nome....: {destinoParam.nome}")
+    logar.write(f"Email...: {destinoParam.email}")
+
     from_address    = "juridico.publicacao@gruposouzalima.com"
 
     to_address = destinoParam.email 
 
     # arquivo JSON
     jsonFile = re.sub(r'\.pdf$', '.json', fileParam )
-    reg = None
+    publicacao = None
     #print( jsonFile )
     with open(  jsonFile , "rb") as jsonConteudo:
-        reg = json.loads(  jsonConteudo.read() )
-        #print( reg['id'] )
-
-    #to_address      = 'flavia.coelho@gruposouzalima.com'
-    #to_address      = 'brenno.cardoso@gruposouzalima.com' 
+        publicacao = json.loads(  jsonConteudo.read() )
+        #print( publicacao['id'] )
 
     cc_address      = []
-    cc_address.append( 'neusa.sotana@gruposouzalima.com' )
-    cc_address.append( 'mariana.gallo@gruposouzalima.com' )
-    cc_address.append( 'luis.bortoletti@gruposouzalima.com' )
+    if( destinoParam.chave == 'controle'):
+        cc_address.append( 'mariana.gallo@gruposouzalima.com' )
+    if( publicacao['horas48'] ):
+        cc_address.append( 'neusa.sotana@gruposouzalima.com' )
+        cc_address.append( 'mariana.gallo@gruposouzalima.com' )
+    """
+    if( not destinoParam.chave == 'dra-neusa'):
+        cc_address.append( 'neusa.sotana@gruposouzalima.com' )
+    if( not destinoParam.chave == 'dra-mariana'):
+        cc_address.append( 'mariana.gallo@gruposouzalima.com' )
+    """
+    # cc_address.append( 'luis.bortoletti@gruposouzalima.com' )
     #cc_address.append( 'brenno.cardoso@gruposouzalima.com' )
 
     # Configurar a mensagem
 
     subject = f"{destino.nome}"
-    subject = f"{subject} - {reg['processo'].replace(':','')}"
-    subject = f"{subject} - {reg['id']} - {getTimestampFmt2()}-{random.randint(1, 10)} "
+    subject = f"{subject} - {publicacao['processo'].replace(':','')}"
+    subject = f"{subject} - {publicacao['id']} - {getTimestampFmt2()}-{random.randint(1, 10)} "
+
+    if( publicacao['horas48'] ):
+        subject = f"*** 48 HORAS *** {subject}"
 
     msg = MIMEMultipart()
     msg['From'] = from_address
@@ -78,19 +102,25 @@ def sendEmail( fileParam, destinoParam ):
         mime_image.add_header('Content-ID', '<logo>')
         msg.attach(mime_image)
 
+    destinos = ''
+    if( destinoParam.chave == 'controle'):
+        destinos = publicacao['destino']
+
     html = f"""
     <html>
     <body>
+        <p><b>{destinos}</b></p>
         <img src="cid:logo">
         <p>Olá, {destinoParam.nome}
-        <br>Processo: {reg['processo']}
+        <br>Processo: {publicacao['processo']}
         </p>
         <hr>
-        <pre>{reg['linha']}</pre>
+        <pre style="font-size: 23px;">{publicacao['linha']}</pre>
     </body>
     </html>
     """
 
+    #logar.write( f"{html}")
 
     # Anexar o corpo da mensagem
     with open( fileParam, "rb") as attachment:
@@ -116,85 +146,31 @@ def sendEmail( fileParam, destinoParam ):
     from_address    = "juridico.publicacao@gruposouzalima.com"
     
 
-    cc_address      = []
-    cc_address.append( 'luis.bortoletti@gruposouzalima.com' )
-    cc_address.append( 'neusa.sotana@gruposouzalima.com' )
-    cc_address.append( 'mariana.gallo@gruposouzalima.com' )
-    #cc_address.append( 'brenno.cardoso@gruposouzalima.com' )
-    #for emailDestino in to_address:
-    # server.sendmail( from_address , to_address, text)
+    logar.write( f"Assunto..: {subject}")
+    logar.write( f"from.....: {from_address}" )
+    logar.write( f"to.......:{[to_address] + cc_address}")
+    #"""
     try:
+
+        time.sleep(2)
+
         server.sendmail( from_address , [to_address] + cc_address, text)
-    
-        print( f"{subject}; de: {from_address} para: {to_address}")
+        logar.write("****************************   EMAIL ENVIADO   ****************************")
         try:
             os.remove( fileParam )
             os.remove( jsonFile )
-        except:
+            logar.write(f"removido: {fileParam}")
+        except Exception as e:
+            logar.write( f"{e}")
             pass
-
-        time.sleep(3)
-    except:
-       pass
-
-
-'''
-    # Lista de arquivos a serem anexados
-
-
-    # Iterar sobre a lista de arquivos e anexar cada um
-    for arquivo in arquivos:
-        filename = f"{diretorio}/{arquivo}"
-        with open(filename, "rb") as attachment:
-            # Criar uma instância MIMEBase e nomear o anexo
-            part = MIMEBase("application", "octet-stream")
-            part.set_payload(attachment.read())
         
-            # Codificar o arquivo em base64
-            encoders.encode_base64(part)
-            
-            # Adicionar o cabeçalho ao arquivo anexado
-            part.add_header(
-                "Content-Disposition",
-                f"attachment; filename= {filename.split('/')[-1]}",  # Extrai apenas o nome do arquivo
-            )
-            
-            # Anexar o arquivo à mensagem
-            msg.attach(part)
 
-
-
-
-    #==============================================
-
-    """
-
-    # Especificar o caminho do arquivo que você deseja anexar
-    filename = "/opt/bitnami/nginx/html/py-email-send/README.md"  # Exemplo: "documento.pdf"
-    filename2 = "/opt/bitnami/nginx/html/py-ocr/src/publicacoes/saida"
-    # Abrir o arquivo no modo binário
-    with open(filename, "rb") as attachment:
-        # Criar uma instância MIMEBase e nomear o anexo
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-
-    # Codificar o arquivo em base64
-    encoders.encode_base64(part)
-
-    # Adicionar o cabeçalho ao arquivo anexado
-    part.add_header(
-        "Content-Disposition",
-        f"attachment; filename= {filename}",
-    )
-
-    # Anexar o arquivo à mensagem
-    msg.attach(part)
-
-    """
-
-
-'''
-
+    except Exception as e:
+       logar.write( f"{e}")
+       pass
+    #"""
+    
+    
 
 
 
@@ -204,16 +180,31 @@ def sendEmail( fileParam, destinoParam ):
 #                      MAIN
 #============================================================
 
+logar = Logger( f"./logs/enviar-email-{getTimestampFmt2()}-{getRandom()}")
+logar.write("====================   INICIO   ========================")
+
+server = smtplib.SMTP('smtp-relay.gmail.com', 587)
+server.starttls()  # Iniciar a conexão TLS
+
+# Conectar ao servidor SMTP do Gmail e enviar o e-mail
+conta_email = "luis.bortoletti@gruposouzalima.com"
+password = "Borto!2024#01"
+server.login( conta_email , password)
+
 destinos = {}
-destinos["dr-alecsandro"] = Destino("Dr Alecsandro", "alecsandro.silva@gruposouzalima.com")
-destinos["dra-ana"] = Destino("Dra Ana", "ana.takahashi@gruposouzalima.com")
-destinos["dra-lais"] = Destino("Dra Lais", "lais.goncalves@gruposouzalima.com")
-destinos["dra-mariana"] = Destino("Dra Mariana", "mariana.gallo@gruposouzalima.com")
-destinos["dra-nelia"] = Destino("Dra Nelia", "nelia.fernandes@gruposouzalima.com")
-destinos["dra-neusa"] = Destino("Dra Neusa", "neusa.sotana@gruposouzalima.com")
-destinos["dr-rafael"] = Destino("Dr Rafael", "rafael.santos@gruposouzalima.com")
-destinos["dra-raquel"] = Destino("Dra Raquel", "rafael.santos@gruposouzalima.com")
-destinos["dra-simone"] = Destino("Dra Simone", "simone.vieira@gruposouzalima.com")
+destinos["controle"] = Destino("controle", "neusa.sotana@gruposouzalima.com","controle")
+destinos["dr-alecsandro"] = Destino("Dr Alecsandro", "alecsandro.silva@gruposouzalima.com","dr-alecsandro")
+destinos["dra-ana"] = Destino("Dra Ana", "ana.takahashi@gruposouzalima.com","dra-ana")
+destinos["dra-lais"] = Destino("Dra Lais", "lais.goncalves@gruposouzalima.com","dra-lais")
+destinos["dra-mariana"] = Destino("Dra Mariana", "mariana.gallo@gruposouzalima.com","dra-mariana")
+destinos["dra-maria-avelino"] = Destino("Dra Maria Avelino", "maria.avelino@gruposouzalima.com","dra-maria-avelino")
+destinos["dra-nelia"] = Destino("Dra Nelia", "nelia.fernandes@gruposouzalima.com","dra-nelia")
+destinos["dra-neusa"] = Destino("Dra Neusa", "neusa.sotana@gruposouzalima.com","dra-neusa")
+destinos["dr-rafael"] = Destino("Dr Rafael", "rafael.santos@gruposouzalima.com","dr-rafael")
+destinos["dra-raquel"] = Destino("Dra Raquel", "raquel.fonseca@gruposouzalima.com","dra-raquel")
+destinos["dra-simone"] = Destino("Dra Simone", "simone.vieira@gruposouzalima.com","dra-simone")
+destinos["dra-vania"] = Destino("Dra Vania", "vania.moura@gruposouzalima.com","dra-simone")
+
 
 lista = []
 lista.append( 'luis.bortoletti@gruposouzalima.com' )
@@ -226,12 +217,12 @@ lista.append( 'mariana.gallo@gruposouzalima.com' )
 
 
 # Especificar o diretório
-diretorio = "/opt/bitnami/nginx/html/py-ocr/src/publicacoes-v2/saida"
+diretorio = "/opt/bitnami/nginx/html/py-ocr/src/publicacoes-v3/saida"
 #diretorio = "/opt/bitnami/nginx/html/py-email-send/tmp"
 
 
 for chave, destino in destinos.items():
-    print( f"{chave} - {destino}")
+    logar.write( f"{chave} - {destino}")
 
     # Listar todos os arquivos no diretório
     arquivos = os.listdir(diretorio)
@@ -239,9 +230,11 @@ for chave, destino in destinos.items():
 
     for arquivo in arquivos:
       if( re.search( chave, arquivo ) ):
-        print( f"{arquivo}")
+        logar.write( f"{arquivo}")
         #destino.email = "luis.bortoletti@gruposouzalima.com"
-        sendEmail( f"{arquivo}", destino  )
+        sendEmail( f"{arquivo}", destino, server, logar  )
 
 
 server.quit()
+
+logar.write("====================   FIM   ========================")
